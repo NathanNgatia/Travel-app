92bfb9e62ce179387a751ca3815d9224b875f152
Samson Kinara <kinaraishima@gmail.com>
2025-04-09 12:40:29 +0300

    search_bar

--- lib/pages/myhomepage.dart
+++ lib/pages/myhomepage.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:herrise_app/widgets/text.dart';
+import 'package:herrise_app/widgets/search_bar.dart' as custom; // ✅ Import the SearchBar with alias
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
- 
 
   final String title;
 
@@ -13,36 +12,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-     
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
         title: Text(widget.title),
       ),
-      body: Center(
-        
+      body: SingleChildScrollView(
         child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-           Image.asset("../assets/images/91236d7f-fb3f-4b80-ac09-ff6e1097686f.jpeg", width: 500, height: 400,),
-            TextWidget(displaytext: 'press 10 times to load our website', styleVariant: 'title', displayText: 'press 10 times to load our website',),
-            TextWidget(displaytext: '$_counter', styleVariant: 'subtitle', displayText: '',),
-             Text(
-              '$_counter',
-              style: TextStyle(fontSize:36,color: Colors.amber,fontWeight: FontWeight.bold),),
-            
+            const SizedBox(height: 30),
+            const custom.SearchBar(), // ✅ Use the aliased SearchBar
+            const SizedBox(height: 20),
+            Center(
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    "../assets/images/91236d7f-fb3f-4b80-ac09-ff6e1097686f.jpeg",
+                    width: 500,
+                    height: 400,
+                  ),
+                  const TextWidget(
+                    displaytext: 'press 10 times to load our website',
+                    styleVariant: 'title',
+                    displayText: 'press 10 times to load our website',
+                  ),
+                  const TextWidget(
+                    displaytext: '',
+                    styleVariant: 'subtitle',
+                    displayText: '',
+                  ),
+                  Text(
+                    '$_counter',
+                    style: const TextStyle(
+                      fontSize: 36,
+                      color: Colors.amber,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -50,17 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
-
-mixin y {
-  // ignore: prefer_typing_uninitialized_variables
-  var center;
-}
-
-// ignore: camel_case_types
-class _counter {
-}
-
