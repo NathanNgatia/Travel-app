0c864adb928117635f54f3c19a019f4045fba509
gord0n0 <itsgordon4@gmail.com>
2025-04-10 11:47:27 +0300

    Navbar

--- lib/pages/myhomepage.dart
+++ lib/pages/myhomepage.dart
@@ -4,8 +4,6 @@ import 'package:herrise_app/widgets/text.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
- 
-
   final String title;
 
   @override
@@ -13,36 +11,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0; // Declare _counter
 
   void _incrementCounter() {
     setState(() {
-     
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
         title: Text(widget.title),
       ),
+      // Add the Drawer widget here
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Navigation Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Home'),
+              onTap: () {
+                // You can navigate to other screens here
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              title: Text('Profile'),
+              onTap: () {
+                // Handle profile navigation here
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              title: Text('Settings'),
+              onTap: () {
+                // Handle settings navigation here
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+          ],
+        ),
+      ),
       body: Center(
-        
         child: Column(
-         
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-           Image.asset("../assets/images/91236d7f-fb3f-4b80-ac09-ff6e1097686f.jpeg", width: 500, height: 400,),
-            TextWidget(displaytext: 'press 10 times to load our website', styleVariant: 'title', displayText: 'press 10 times to load our website',),
-            TextWidget(displaytext: '$_counter', styleVariant: 'subtitle', displayText: '',),
-             Text(
+            Image.asset(
+              "../assets/images/91236d7f-fb3f-4b80-ac09-ff6e1097686f.jpeg",
+              width: 500,
+              height: 400,
+            ),
+            TextWidget(
+              displaytext: 'press 10 times to load our website',
+              styleVariant: 'title',
+              displayText: 'press 10 times to load our website',
+            ),
+            TextWidget(
+              displaytext: '$_counter',
+              styleVariant: 'subtitle',
+              displayText: '',
+            ),
+            Text(
               '$_counter',
-              style: TextStyle(fontSize:36,color: Colors.amber,fontWeight: FontWeight.bold),),
-            
+              style: TextStyle(
+                  fontSize: 36,
+                  color: Colors.amber,
+                  fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
@@ -61,6 +111,6 @@ mixin y {
 }
 
 // ignore: camel_case_types
-class _counter {
-}
+class _counter {}
+
 
